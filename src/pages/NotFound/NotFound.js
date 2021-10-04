2bb1ac48c6899ea8d156ba8a026b53c45158fb3c
Didar2255 <didar.a2255@gmail.com>
2021-10-04 18:03:03 +0600

    ass usehistory

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import './NotFound.css'
+import { useHistory } from "react-router-dom";
+
 
 const NotFound = () => {
+    const history = useHistory()
+    const hendelClick = () => {
+        history.push('/home')
+    }
     return (
         <div className='error'>
             <img src="https://vetra.laborasyon.com/assets/svg/404.svg" alt="" />
             <h1>Page not found</h1>
             <p>The page you want to go is not currently available</p>
-            <Link to='/home'>
-                <Button className='bg-danger border border-0'>Home</Button>
-            </Link>
+            <Button className='bg-danger border border-0' onClick={hendelClick}>Home</Button>
         </div>
     );
 };
